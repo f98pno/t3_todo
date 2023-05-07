36ef31cf67fbe04306a966a57eceb942deb8956f
Peter Novak <peter.novak.m@gmail.com>
2023-05-07 16:24:15 +0800

    Adding delete todos

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -16,6 +16,12 @@ export default function Todo({ todo }: TodoProps) {
     },
   });
 
+  const { mutate: deleteMutation } = api.todo.delete.useMutation({
+    onSettled: async () => {
+      await trpc.todo.all.invalidate();
+    },
+  });
+
   return (
     <>
       <div className="flex items-center justify-between gap-2">
@@ -37,7 +43,12 @@ export default function Todo({ todo }: TodoProps) {
             {text}
           </label>
         </div>
-        <button className="w-full rounded-lg bg-blue-700 px-2 py-1 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">
+        <button
+          className="w-full rounded-lg bg-blue-700 px-2 py-1 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
+          onClick={() => {
+            deleteMutation(id);
+          }}
+        >
           Delete
         </button>
       </div>
